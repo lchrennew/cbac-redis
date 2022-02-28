5e75d0bfa2d418b06a50e1b13e6ffe3f49fff8f6
lichun <lchrennew@126.com>
2022-02-28 09:56:18 +0800

    fix: miss es-object-id

--- src/core/aliases.js
+++ src/core/aliases.js
@@ -1,6 +1,6 @@
 import * as keys from './redis-keys.js'
 import { redis } from "../utils/redis.js";
-import { generateObjectID } from "../utils/oid.js";
+import { generateObjectID } from "es-object-id";
 
 export const getClaimSha256sByAliases = (...aliases) => redis.hmget(keys.validatorAliases(), ...aliases)
 export const existsAlias = alias => redis.hexists(keys.validatorAliases(), alias)
